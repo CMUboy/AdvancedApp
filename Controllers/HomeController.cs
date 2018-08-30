d026ea50278127c651d7ada8fa0da05d5307200d
Joseph Ku <cmuboy@yahoo.com>
2018-08-30 01:18:10 -0600

    Chapter 19: Working with Keys

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,43 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AdvancedApp.Models;
 using Microsoft.AspNetCore.Mvc;
-using AdvancedApp.Models;
 
-namespace AdvancedApp.Controllers
-{
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
+namespace AdvancedApp.Controllers {
+
+    public class HomeController : Controller {
+        private AdvancedContext context;
+
+        public HomeController(AdvancedContext ctx) => context = ctx;
+
+        public IActionResult Index() {
+            return View(context.Employees);
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+        public IActionResult Edit(long id) {
+            return View(id == default(long)
+                ? new Employee() : context.Employees.Find(id));
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
+        [HttpPost]
+        public IActionResult Update(Employee employee) {
 
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (employee.Id == default(long)) {
+                context.Add(employee);
+            } else {
+                context.Update(employee);
+            }
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
